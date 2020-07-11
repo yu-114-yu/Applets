74706e3e35a7ee07dc6a8f62f06f4fc8f3fab984
Yu <15880377720@163.com>
2020-07-11 09:01:53 +0800

    注册小程序和页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,51 +4,39 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    name: 'yujias',
+    stars: [
+      {id: 1, name: 'a', age: 18},
+      {id: 2, name: 'b', age: 12},
+      {id: 3, name: 'c', age: 15},
+      {id: 4, name: 'd', age: 16}
+    ],
+    counter: 0
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  count() {
+    // this.data.counter++
+    // console.log(this.data.counter);
+    
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      counter: this.data.counter + 1
     })
+  },
+  jian() {
+    this.setData({
+      counter: this.data.counter - 1
+    })
+  },
+  handle(event) {
+    console.log(event);
+  },
+  onLoad() {
+    // wx.request({
+    //   url: 'http://123/207.32.32:8000/recommend',
+    //   success: (res) => {
+    //     console.log(res);
+        
+    //   }
+    // })
   }
 })
+
